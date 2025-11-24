13983d3f44d80ee0d024bf0ee48e0f1d225021fe
ajpsycho13 <ajayjoseph24@gmail.com>
2025-11-24 11:32:09 +0530

    Update a.py

--- a.py
+++ a.py
@@ -4,11 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "hai"
 
 @app.route("/a")
 def second():
-    return render_template("second.html")
+    return "hello"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
